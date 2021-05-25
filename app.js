3accf20c4e084176e22270401243a654f0a4b455
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-25 10:08:38 +0200

    function pause

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
-const button = document.querySelector('#button');
-var time;
+const heures   = document.querySelector('#heures'); 
+const minutes  = document.querySelector('#minutes') ;
+const secondes = document.querySelector('#secondes'); 
+const button   = document.querySelector('#button');
+const button2  = document.querySelector('#button2');
 
- function chronometre () {
-  let heures= document.querySelector('#heures'); 
-  let minutes= document.querySelector('#minutes') ;
-  let secondes = document.querySelector('#secondes'); 
+let time;
 
+
+
+ function chronometre () { 
   let sec = 0
   let min = 0
   let heur = 0
@@ -16,22 +19,28 @@ var time;
       sec = 0;
       min ++;
       minutes.textContent = min;
-      if (min == 60) {
-        min = 0;
-        heur++;
-        heures.textContent = heur; 
-
-    } 
-   
-
-    }
-   secondes.textContent = sec;
+         
+    } else if (min == 60) {
+      min = 0;
+      heur++;
+      heures.textContent = heur; 
+    }   
+  secondes.textContent = sec;
     sec++;  
-  }, 1000);
-}  
+  }, 10);
+};   
 
 
-button.addEventListener('click', function(){
-  chronometre()}); 
+button.addEventListener('click', ()=>{
+  button.classList.add("d-none");
+  button2.classList.remove("d-none"); 
+  chronometre()
+}); 
+
+button2.addEventListener('click', ()=>{
+  clearInterval(time);
+  button.classList.remove("d-none");
+  button2.classList.add("d-none");
+})
   
  
