254baa5aaa2ba45acdb1314e293a7ea2f47cfad7
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-25 14:48:22 +0200

    correction de mls

--- app.js
+++ app.js
@@ -7,39 +7,42 @@ const button       = document.querySelector('#button');
 const button2      = document.querySelector('#button2');
 const reset        = document.querySelector('#reset');
 
-let mls = 0
-let s  = 0
-let m  = 0
-let h  = 0
+let mls = 00;
+let s   = 00;
+let m   = 00;
+let h   = 00;
 let timeStart;
 
 function chronometre () {   
-  timeStart = setInterval(() => {
+
+
+ timeStart = setInterval(() => {
     
-   
+    mls++; 
+    milisecondes.innerHTML = '0' + mls; 
 
     if(mls > 9){
       s++;
-      msl = 0;      
+      mls = 0;      
       secondes.innerHTML = s;
     }
+
     if (s > 59){
       s = 0;
       m ++;
-      minutes.textContent = m;
+      minutes.innerHTML = '0'+m;
     }         
     if (m > 59) {
       m = 0;
       h++;
-      heures.textContent = h; 
+      heures.innerHTML = h; 
     }   
     if (h> 24){
       h = 0;
     }
   
-    mls++; 
-    milisecondes.textContent = mls; 
-  }, 1000);
+  
+  }, 100);
 };   
 
 button.addEventListener('click', ()=>{
@@ -54,7 +57,6 @@ button2.addEventListener('click', ()=>{
   button2.classList.add("d-none");
 })
   
- 
 reset.addEventListener('click', ()=>{
   location.reload();
 })
